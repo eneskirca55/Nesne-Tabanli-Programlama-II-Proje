96ab9c054f89e9875a69c822702b3ca580ff850d
Muhammed Enes Kırca <kircamuhammedenes@gmail.com>
2024-06-22 17:35:06 +0300

    Nesne Tabanlı Programlama Proje
    python flask uygulaması

---
+++ nesneproje/app/forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+import sqlalchemy as sa
+from app import db
+from app.models import User
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Kullanıcı Adı', validators=[DataRequired()])
+    password = PasswordField('Şifre', validators=[DataRequired()])
+    remember_me = BooleanField('Beni Hatırla')
+    submit = SubmitField('Giriş Yap')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Kullanıcı Adı', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Şifre', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Tekrar Şifre', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Kayıt Ol')
+
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(
+            User.username == username.data))
+        if user is not None:
+            raise ValidationError('Lütfen farklı bir kullanıcı adı kullanın.')
+
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(
+            User.email == email.data))
+        if user is not None:
+            raise ValidationError('Lütfen farklı bir email adresi kullanın.')
